7f7bfc38d453c35f0fcf1d43a06f3eea2e9a5f54
Kyle Bulloch <student@epicodus.com>
2015-04-24 10:02:16 -0700

    Add home.html skeleton and route

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var signInApp = angular.module('signInApp', ['ui.router']);
+
+signInApp.config(function($stateProvider) {
+
+  $stateProvider.state('home', {
+    url: '',
+    templateUrl: 'partials/home.html'
+  });
+
+});
