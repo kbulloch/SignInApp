56841487bbe07781eab2824ebe84e927f2c41277
Kyle Bulloch <student@epicodus.com>
2015-04-24 11:25:57 -0700

    Add custom css

--- services/AttendenceFactory.js
+++ services/AttendenceFactory.js
@@ -10,6 +10,11 @@ signInApp.factory('AttendenceFactory', function AttendenceFactory() {
     { name: "Sordi Timothy-French", signed_in: false },
     { name: "Patrick The Viking", signed_in: false },
     { name: "Linus Torvalds", signed_in: false },
+    { name: "Balthor Hammersmith", signed_in: false },
+    { name: "Beardly Ericsson", signed_in: false },
+    { name: "Usef Darkheart", signed_in: false },
+    { name: "Red Foreman", signed_in: false },
+    { name: "Slappy", signed_in: false },
     { name: "Smooty McCoodle", signed_in: false },
     { name: "Morewood Graybles", signed_in: false }
   ];
